a734e759538ea2ceec70f6d21fc47d4538ca1f5a
NateMcCurdy <nate.mccurdy@puppetlabs.com>
2015-01-15 12:11:09 -0800

    Re-order tasks to install homebrew first.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Install Dependencies
+# Setup HomeBrew
+#   - Install brews and casks and stuff.
+# Install Dependencies using brew and others.
 #   - What are they???
 # Setup Homesick and link dotfiles.
 #   - Install Homesick.
 #   - Setup my dotfiles repo and link files.
-# Setup HomeBrew
-#   - Install brews and casks and stuff.
 # Run config tasks
 #
