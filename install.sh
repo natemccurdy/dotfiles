76a0765b84126aaf2704b059cff8fe3c75df13d0
NateMcCurdy <nate.mccurdy@puppetlabs.com>
2015-01-15 11:39:14 -0800

    Add install.sh file to be run from curl on a fresh install.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install Dependencies
+#   - What are they???
+# Setup Homesick and link dotfiles.
+#   - Install Homesick.
+#   - Setup my dotfiles repo and link files.
+# Setup HomeBrew
+#   - Install brews and casks and stuff.
+# Run config tasks
+#
