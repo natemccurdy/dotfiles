d9990ee765bcd7975d00cc254e60e24c0b1a49fd
NateMcCurdy <nate.mccurdy@puppetlabs.com>
2015-01-16 10:36:46 -0800

    Do colors right during install. Better organization.

--- install.sh
+++ install.sh
@@ -2,21 +2,17 @@
 
 #===================================================
 # Some helper functions. Please ignore.
-#   credit: https://github.com/atomantic/dotfiles
 #===================================================
 # Colors
-ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
-COL_RED=$ESC_SEQ"31;01m"
-COL_GREEN=$ESC_SEQ"32;01m"
-function ok() {
-    echo -e "$COL_GREEN[ok]$COL_RESET "$1
-}
-function bot() {
-    echo -e "\n$COL_GREEN\[._.]/$COL_RESET - "$1
+ESC="\x1b["
+RESET=$ESC"39;49;00m"
+RED=$ESC"31;01m"
+GREEN=$ESC"32;01m"
+function info() {
+    echo -e "\n${GREEN}[INFO] ${1}${RESET}"
 }
 function error() {
-    echo -e "$COL_RED[error]$COL_RESET "$1
+    echo -e "\n${RED}[ERROR] ${1}${RESET}"
 }
 #===================================================
 # End of helper functions. Carry on.
@@ -24,50 +20,57 @@ function error() {
 
 
 # Ask for the administrator password upfront
-bot "I need you to enter your sudo password so I can install some things:"
-sudo -v; ok
+info "I need you to enter your sudo password so I can install some things:"
+sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until this has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Update OSX's RubyGems
-bot "Updating OSX's RubyGems before doing anything else..."
+info "Updating OSX's RubyGems before doing anything else..."
 sudo gem update --system
 
 # Setup Homesick and pull down the dotfiles repo
 # so that we have access to the Rakefile.
 castle="natemccurdy/dotfiles"
-bot "Install Homesick and pull down $castle"
+info "Install Homesick and pull down $castle"
 sudo gem install homesick
 if ! which homesick >/dev/null 2>&1; then
-    error "Uh oh, it looks like Homesick didn't install."
+    error "Uh oh, it looks like Homesick didn't install. Exiting..."
     exit 1
 fi
 if [[ $(homesick list) =~ $castle ]]; then
-    bot "$castle already exists. Let's just update it then..."
+    info "$castle already exists. Let's just update it then..."
     homesick pull dotfiles
     homesick symlink dotfiles
 else
-    bot "Pulling down $castle..."
+    info "Pulling down $castle..."
     homesick clone $castle
-    bot "Symlinking $castle..."
+    info "Symlinking $castle..."
     homesick symlink dotfiles
+fi
+
+# Install gems from the Gemfile using bundler.
+gemfile="$(homesick show_path dotfiles)/Gemfile"
+info "Attempting to install all of our gems now"
+if [[ -f $gemfile ]]; then
+    info "Gemfile found. Installing Ruby Gems from $gemfile"
+    cd $(homesick show_path)
+    bundle install
+else
+    error "Can't find the Gemfile at '${gemfile}'. Exiting..."
+    exit 1
 fi
 
 # Run the Rakefile from here on out.
 rakefile="$(homesick show_path dotfiles)/Rakefile"
+info "Attempting to run the Rakefile to continue with the install"
 if [[ -f $rakefile ]]; then
-    bot "Awesome, good to go. Let's run the Rakefile now..."
+    info "Rakefile found. Running $rakefile"
     cd $(homesick show_path)
     rake install
 else
-    error "Can't find the Rakefile at $rakefile"
+    error "Can't find the Rakefile at '${rakefile}'. Exiting..."
     exit 1
 fi
 
-# Setup HomeBrew
-#   - Install brews and casks and stuff.
-# Install Dependencies using brew and others.
-#   - What are they???
-# Run config tasks
-#
