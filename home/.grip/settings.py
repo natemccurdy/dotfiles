e5b99b445da9a1b2b0d938ca81a775911593773a
NateMcCurdy <nate.mccurdy@puppetlabs.com>
2015-07-27 15:13:19 -0700

    Use token for Grip auth. Find in keychain.

--- home/.grip/settings.py
+++ home/.grip/settings.py
@@ -1,12 +1,11 @@
-def find_password():
+def find_token():
     import subprocess
     import re
-    cmd = ['security', 'find-internet-password', '-gs', 'github.com']
+    cmd = ['security', 'find-generic-password', '-gs', 'grip_github_token']
     pwinfo = subprocess.Popen(cmd, stdout = subprocess.PIPE,
             stderr = subprocess.PIPE)
     pwline = pwinfo.stderr.read().strip()
     return re.sub('password: "(.*)"', '\\1', pwline)
 
 
-PASSWORD = find_password()
-USERNAME = 'nate.mccurdy@puppetlabs.com'
+PASSWORD = find_token()
