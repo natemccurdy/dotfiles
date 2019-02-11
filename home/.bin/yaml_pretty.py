03b613ed5acaaf38adc0dc538bc573a2d7870bdd
Nate McCurdy <mccurdy.nate@gmail.com>
2019-02-11 11:45:50 -0800

    Add a YAML pretty-printer and shortcut in VIM
    This requires `pip3 install ruamel.yaml`

---
+++ home/.bin/yaml_pretty.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from ruamel.yaml import YAML
+import sys
+
+yaml=YAML()
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.default_flow_style = False
+yaml.explicit_start = True
+
+yaml.dump(yaml.load(sys.stdin), sys.stdout)
