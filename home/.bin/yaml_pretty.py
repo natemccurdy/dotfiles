0dc2b4431be6c1a50b2695c89e1ecfe0042daf19
Nate McCurdy <mccurdy.nate@gmail.com>
2019-02-11 12:05:08 -0800

    yaml_pretty.py: Preserve quotes

--- home/.bin/yaml_pretty.py
+++ home/.bin/yaml_pretty.py
@@ -6,5 +6,6 @@ yaml=YAML()
 yaml.indent(mapping=2, sequence=4, offset=2)
 yaml.default_flow_style = False
 yaml.explicit_start = True
+yaml.preserve_quotes = True
 
 yaml.dump(yaml.load(sys.stdin), sys.stdout)
