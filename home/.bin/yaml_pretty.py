101a7c63e6ca02a699eae1114c9efc9c61bcca1d
Nate McCurdy <mccurdy.nate@gmail.com>
2019-04-02 13:07:09 -0700

    YAMLPretty: represent nulls as ~

--- home/.bin/yaml_pretty.py
+++ home/.bin/yaml_pretty.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 import sys
 try:
-    from ruamel.yaml import YAML
+    import ruamel.yaml
 except ImportError:
     sys.exit('Error, ruamel library not found. Try: pip3 install ruamel.yaml')
 
-yaml = YAML()
+yaml = ruamel.yaml.YAML
 yaml.indent(mapping=2, sequence=4, offset=2)
 yaml.default_flow_style = False
 yaml.explicit_start = True
 yaml.preserve_quotes = True
+
+
+# Represent null values as ~ instead of blanks
+# https://stackoverflow.com/a/44314840
+def custom_null(self, data):
+    return self.represent_scalar(u'tag:yaml.org,2002:null', u'~')
+
+
+ruamel.yaml.RoundTripRepresenter.add_representer(type(None), custom_null)
 
 yaml.dump(yaml.load(sys.stdin), sys.stdout)
