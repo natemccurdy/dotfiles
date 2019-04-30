f6eb4b1688e422f54939932ab0956e85758e0b8c
Nate McCurdy <mccurdy.nate@gmail.com>
2019-04-30 15:07:04 -0700

    Fix bug in yaml_pretty.py

--- home/.bin/yaml_pretty.py
+++ home/.bin/yaml_pretty.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     sys.exit('Error, ruamel library not found. Try: pip3 install ruamel.yaml')
 
-yaml = ruamel.yaml.YAML
+yaml = ruamel.yaml.YAML()
 yaml.indent(mapping=2, sequence=4, offset=2)
 yaml.default_flow_style = False
 yaml.explicit_start = True
