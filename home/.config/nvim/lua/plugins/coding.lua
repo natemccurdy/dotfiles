e4da5664164a22b5bf8afca2d517fe40c9c44eb3
Nate McCurdy <mccurdy.nate@gmail.com>
2025-01-15 18:11:55 -0800

    nvim: replace mini.pairs with nvim-autopairs, rm old nvim-cmp config

--- home/.config/nvim/lua/plugins/coding.lua
+++ home/.config/nvim/lua/plugins/coding.lua
@@ -1,70 +1,10 @@
 return {
+  -- https://github.com/LazyVim/LazyVim/discussions/2248
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-    },
-    opts = function(_, opts)
-      -- Add emoji completion
-      table.insert(opts.sources, { name = "emoji" })
-
-      -- Autocomplete buffer keywords from all open buffers and when other
-      -- completion sources (like LSP and copilot) are active.
-      table.insert(opts.sources, 1, {
-        name = "buffer",
-        option = {
-          get_bufnrs = function()
-            return vim.api.nvim_list_bufs()
-          end,
-        },
-      })
-
-      local cmp = require("cmp")
-
-      -- Don't auto-select the first completion option. This allows for
-      -- pressing enter (<CR>) to make a new line.
-      -- https://github.com/LazyVim/LazyVim/issues/ 3912
-      opts.preselect = cmp.PreselectMode.None
-      opts.completion = { completeopt = "menu,menuone,preview,noinsert,noselect" }
-      opts.experimental.ghost_text = false
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      })
-
-      -- Use Tab/S-Tab for selecting completions.
-      -- https://www.lazyvim.org/configuration/recipes#supertab
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- https://github.com/windwp/nvim-autopairs?tab=readme-ov-file#default-values
+    opts = {},
   },
 }
--- home/.config/nvim/lua/plugins/disabled.lua
+++ home/.config/nvim/lua/plugins/disabled.lua
@@ -1,1 +1,5 @@
-return {}
+return {
+  -- Replace mini.pairs with nvim-autopairs
+  -- https://github.com/LazyVim/LazyVim/discussions/2248
+  { "echasnovski/mini.pairs", enabled = false },
+}
